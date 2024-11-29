444ea7cdfbe3cfd9b30ecbb44280f0ad5dd5f5fe
gcho3223 <guk.cho3223@gmail.com>
2024-11-30 05:49:48 +0900

    .

---
+++ AnalysisCode/testrun.sh
@@ -0,0 +1,18 @@
+#!/bin/tcsh
+
+#set inputlists = ("Data_DoubleMuon_Run2016B_1")
+#set inputlists = ("Data_DoubleMuon_Run2016G_1")
+
+# ttbar
+#./ssb_analysis TTJets_Signal/TTJets_Signal_1.list TTJets_Signal_1_GenInfo.root 0 TTJets_Signal
+
+# CEDM sample
+#./ssb_analysis CEDM_Sample/TTJets_Signal_dtG_0/TTJets_Signal_dtG_0.list ttbar_v6/CEDM/v5/TTJets_Signal_dtG_0.root 0 TTJets_Signal_dtG_0
+#./ssb_analysis CEDM_Sample/TTJets_Signal_dtG_0p5207/TTJets_Signal_dtG_0p5207.list ttbar_v6/CEDM/v5/TTJets_Signal_dtG_0p5207.root 0 TTJets_Signal_dtG_0p5207
+#./ssb_analysis CEDM_Sample/TTJets_Signal_dtG_1p0415/TTJets_Signal_dtG_1p0415.list ttbar_v6/CEDM/v5/TTJets_Signal_dtG_1p0415.root 0 TTJets_Signal_dtG_1p0415
+#./ssb_analysis CEDM_Sample/TTJets_Signal_dtG_2p60364/TTJets_Signal_dtG_2p60364.list ttbar_v6/CEDM/v5/TTJets_Signal_dtG_2p60364.root 0 TTJets_Signal_dtG_2p60364
+#./ssb_analysis CEDM_Sample/TTJets_Signal_dtG_m0p5207/TTJets_Signal_dtG_m0p5207.list ttbar_v6/CEDM/v5/TTJets_Signal_dtG_m0p5207.root 0 TTJets_Signal_dtG_m0p5207
+#./ssb_analysis CEDM_Sample/TTJets_Signal_dtG_m1p0415/TTJets_Signal_dtG_m1p0415.list ttbar_v6/CEDM/v5/TTJets_Signal_dtG_m1p0415.root 0 TTJets_Signal_dtG_m1p0415
+#./ssb_analysis CEDM_Sample/TTJets_Signal_dtG_m2p60364/TTJets_Signal_dtG_m2p60364.list ttbar_v6/CEDM/v5/TTJets_Signal_dtG_m2p60364.root 0 TTJets_Signal_dtG_m2p60364
+
+./ssb_analysis CEDM_Sample/TTJets_Signal_dtG_0/TTJets_Signal_dtG_0_1.list TTJets_Signal_dtG_0_v6_O3v2_2.root 0 TTJets_Signal_dtG_0
