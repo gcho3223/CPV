a8e147dd15220974e74403ba0203fb3640b66b89
gcho3223 <guk.cho3223@gmail.com>
2024-12-07 19:55:38 +0900

    updated run summary generator: can use CPV&Dataset smaples at the same time

--- AnalysisCode/Condor/run_summary.sh
+++ AnalysisCode/Condor/run_summary.sh
@@ -2,36 +2,36 @@
 
 samplelist=( 
     ### Data & MC Samples ###
-    #"Data_DoubleMuon_Run2016B" 
-    #"Data_DoubleMuon_Run2016C" 
-    #"Data_DoubleMuon_Run2016D" 
-    #"Data_DoubleMuon_Run2016E" 
-    #"Data_DoubleMuon_Run2016F" 
-    #"Data_DoubleMuon_Run2016G" 
-    #"Data_DoubleMuon_Run2016HV2" 
-    #"Data_DoubleMuon_Run2016HV3" 
-    #"Data_SingleMuon_Run2016B" 
-    #"Data_SingleMuon_Run2016C" 
-    #"Data_SingleMuon_Run2016D" 
-    #"Data_SingleMuon_Run2016E" 
-    #"Data_SingleMuon_Run2016F" 
-    #"Data_SingleMuon_Run2016G" 
-    #"Data_SingleMuon_Run2016HV2" 
-    #"Data_SingleMuon_Run2016HV3" 
-    #"DYJetsToLL_M_10To50" 
+    "Data_DoubleMuon_Run2016B" 
+    "Data_DoubleMuon_Run2016C" 
+    "Data_DoubleMuon_Run2016D" 
+    "Data_DoubleMuon_Run2016E" 
+    "Data_DoubleMuon_Run2016F" 
+    "Data_DoubleMuon_Run2016G" 
+    "Data_DoubleMuon_Run2016HV2" 
+    "Data_DoubleMuon_Run2016HV3" 
+    "Data_SingleMuon_Run2016B" 
+    "Data_SingleMuon_Run2016C" 
+    "Data_SingleMuon_Run2016D" 
+    "Data_SingleMuon_Run2016E" 
+    "Data_SingleMuon_Run2016F" 
+    "Data_SingleMuon_Run2016G" 
+    "Data_SingleMuon_Run2016HV2" 
+    "Data_SingleMuon_Run2016HV3" 
+    "DYJetsToLL_M_10To50" 
     #"DYJetsToLL_M_50" 
-    #"ST_tW_antitop" 
+    "ST_tW_antitop" 
     #"ST_tW_top" 
-    #"TTbar_WJetToLNu" 
-    #"TTbar_WQQ" 
-    #"TTbar_ZQQ" 
+    "TTbar_WJetToLNu" 
+    "TTbar_WQQ" 
+    "TTbar_ZQQ" 
     #"TTbar_ZToLLNuNu" 
     #"TTJets_others" 
     #"TTJets_Signal" 
     #"WJetsToLNu" 
-    #"WW" 
-    #"WZ" 
-    #"ZZ" 
+    "WW" 
+    "WZ" 
+    "ZZ" 
     ### CPV samples ### 
     "TTJets_Signal_dtG_0" 
     "TTJets_Signal_dtG_0p5207" 
@@ -41,27 +41,58 @@ samplelist=(
     "TTJets_Signal_dtG_m1p0415" 
     "TTJets_Signal_dtG_m2p60364" 
 )
-
+######################################################################
+### before running, should change samples you want to make summary ###
+### you can use CPV samples or Dataset samples at the same time    ###
+######################################################################
+###########################
 ### version information ###
-### should change sample_path when CPV samples are used!!! ###
+###########################
 version="v6_O3v2_2"
-#sample_path="./${version}/Dataset"
-sample_path="./${version}/CPV_Sample"
 
-### calc whole of log files from samplelist for progress bar ###
+#########################################################
+### set up sample_path                                ###
+### calculate total # of log files                    ###
+### set up log file path & variables for progress bar ###
+#########################################################
+### progress bar variables ###
 total_sample=0
 completed=0
+### set up sample_path & calculate total # of log files ###
 for i in "${samplelist[@]}"
 do
+    # CPV 샘플인지 확인
+    if [[ $i == TTJets_Signal_dtG_* ]]; then
+        sample_path="./${version}/CPV_Sample"
+    else
+        sample_path="./${version}/Dataset"
+    fi
+
+    # 로그 파일 경로 설정 및 파일 개수 계산
     log_path="${sample_path}/${i}/log_condor/out"
     file_count=$(ls "$log_path"/*.out 2>/dev/null | wc -l)
-    total_samples=$((total_samples + file_count))  #sum of total log files
+    total_sample=$((total_sample + file_count))  #sum of total log files
 done
-
-### start summary !! ###
+# total_sample가 0인지 확인
+if (( total_sample == 0 )); then
+    echo "No log files found. Exiting."
+    exit 1
+fi
+#########################################
+###        start summary !!           ###
+#########################################
 for i in "${samplelist[@]}"
 do
-    ### make directory & initiate txt files ###
+    ################################################
+    ###   check sample name & set up make dir    ###
+    ################################################
+    # CPV 샘플인지 확인
+    if [[ $i == TTJets_Signal_dtG_* ]]; then
+        sample_path="./${version}/CPV_Sample"
+    else
+        sample_path="./${version}/Dataset"
+    fi
+    # 디렉토리 및 파일 생성
     mkdir -p ./Run_Summary/${sample_path}/${i}
     > ./Run_Summary/${sample_path}/${i}/Run_Summary_${i}.txt
     > ./Run_Summary/${sample_path}/${i}/Memory_Summary_${i}.txt
@@ -69,7 +100,9 @@ do
     log_path="${sample_path}/${i}/log_condor/log"
     out_path="${sample_path}/${i}/log_condor/out"
     err_path="${sample_path}/${i}/log_condor/err"
-    ## calc # of log files ##
+    ##########################################  
+    ###       calc # of log files         ###
+    ##########################################
     file_count=$(ls "$out_path"/*.out 2>/dev/null | wc -l)
     # skip if there is no logfile -> to obtail the number of queue
     if (( file_count == 0 )); then
@@ -77,16 +110,23 @@ do
         continue
     fi
     failed_evt=()
+    ##########################################
+    ###      write summary to txt files    ###
+    ##########################################
     ## indicate Sample name ##
     echo "[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[ ${i} ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]" >> "./Run_Summary/${sample_path}/${i}/Run_Summary_${i}.txt"
     echo "[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[ ${i} ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]" >> "./Run_Summary/${sample_path}/${i}/Memory_Summary_${i}.txt"
     for ((j=0; j<file_count; j++))
     do 
-        ## memory summary ##
+        ##########################################
+        ###       memory summary              ###
+        ##########################################
         echo ">> $j th-----------------------------------------------------------------------------------" >> "./Run_Summary/${sample_path}/${i}/Memory_Summary_${i}.txt"
         echo "                              	Usage    Requested   Allocated" >> "./Run_Summary/${sample_path}/${i}/Memory_Summary_${i}.txt"
         echo "`grep " Memory (MB)" "$log_path/log_$j.log"`" >> "./Run_Summary/${sample_path}/${i}/Memory_Summary_${i}.txt"""
-        ## err, out ,log summary ##
+        ##########################################
+        ###       err, out ,log summary        ###
+        ##########################################
         if ! grep -q "exit-code 0" "$log_path/log_$j.log" || grep -q "killed" "$err_path/err_$j.err" || ! grep -q "End processing" "$out_path/out_$j.out"; then
             echo ">> $j th-----------------------------------------------------------------------------------" >> "./Run_Summary/${sample_path}/${i}/Run_Summary_${i}.txt"
             # exit-code 확인 : exit-code 0(정상종료)이 아니면 기록
@@ -107,10 +147,12 @@ do
             fi
             failed_evt[$j]=$j 
         fi
-        ### progress bar ###
+        ################################
+        ###       progress bar       ###
+        ################################
         ((completed++))
         echo -ne "Progress in $i: ["
-        progress=$(( (completed * 100) / total_samples )) 
+        progress=$(( (completed * 100) / total_sample )) 
         for ((k=0; k<=100; k+=2))
         do
             if [ $k -le $progress ]
