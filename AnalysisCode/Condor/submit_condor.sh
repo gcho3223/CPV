3b9d2baf9e250b9a5d5d3e8d4ff66a347ce00620
gcho3223 <guk.cho3223@gmail.com>
2024-12-13 20:30:22 +0900

    condor submit script updated

--- AnalysisCode/Condor/submit_condor.sh
+++ AnalysisCode/Condor/submit_condor.sh
@@ -2,22 +2,22 @@
 
 samplelist=( 
     ### Data & MC Samples ###
-    "Data_DoubleMuon_Run2016B" 
-    "Data_DoubleMuon_Run2016C" 
-    "Data_DoubleMuon_Run2016D" 
-    "Data_DoubleMuon_Run2016E" 
-    "Data_DoubleMuon_Run2016F" 
-    "Data_DoubleMuon_Run2016G" 
-    "Data_DoubleMuon_Run2016HV2" 
-    "Data_DoubleMuon_Run2016HV3" 
-    "Data_SingleMuon_Run2016B" 
-    "Data_SingleMuon_Run2016C" 
-    "Data_SingleMuon_Run2016D" 
-    "Data_SingleMuon_Run2016E" 
-    "Data_SingleMuon_Run2016F" 
-    "Data_SingleMuon_Run2016G" 
-    "Data_SingleMuon_Run2016HV2" 
-    "Data_SingleMuon_Run2016HV3" 
+    #"Data_DoubleMuon_Run2016B" 
+    #"Data_DoubleMuon_Run2016C" 
+    #"Data_DoubleMuon_Run2016D" 
+    #"Data_DoubleMuon_Run2016E" 
+    #"Data_DoubleMuon_Run2016F" 
+    #"Data_DoubleMuon_Run2016G" 
+    #"Data_DoubleMuon_Run2016HV2" 
+    #"Data_DoubleMuon_Run2016HV3" 
+    #"Data_SingleMuon_Run2016B" 
+    #"Data_SingleMuon_Run2016C" 
+    #"Data_SingleMuon_Run2016D" 
+    #"Data_SingleMuon_Run2016E" 
+    #"Data_SingleMuon_Run2016F" 
+    #"Data_SingleMuon_Run2016G" 
+    #"Data_SingleMuon_Run2016HV2" 
+    #"Data_SingleMuon_Run2016HV3" 
     #"DYJetsToLL_M_10To50" 
     #"DYJetsToLL_M_50" 
     #"ST_tW_antitop" 
@@ -33,17 +33,17 @@ samplelist=(
     #"WZ" 
     #"ZZ"
     ### CPV samples ###
-    #"TTJets_Signal_dtG_0" 
-    #"TTJets_Signal_dtG_0p5207" 
-    #"TTJets_Signal_dtG_1p0415" 
-    #"TTJets_Signal_dtG_2p60364" 
-    #"TTJets_Signal_dtG_m0p5207" 
-    #"TTJets_Signal_dtG_m1p0415" 
-    #"TTJets_Signal_dtG_m2p60364" 
+    "TTJets_Signal_dtG_0" 
+    "TTJets_Signal_dtG_0p5207" 
+    "TTJets_Signal_dtG_1p0415" 
+    "TTJets_Signal_dtG_2p60364" 
+    "TTJets_Signal_dtG_m0p5207" 
+    "TTJets_Signal_dtG_m1p0415" 
+    "TTJets_Signal_dtG_m2p60364" 
 )
 ### version information ( Dataset / CPV_Sample ) ###
-#version="Job_Version/v6_O3v2_3/CPV_Sample"
-version="Job_Version/v6_O3v2_3/Dataset"
+version="Job_Version/v6_O3v2_3/CPV_Sample"
+#version="Job_Version/v6_O3v2_3/Dataset"
 ### submit jobs for each sample ###
 for sample in "${samplelist[@]}";
 do
@@ -53,6 +53,6 @@ do
     else
         condor_submit "condor_sub.sub"
     fi
-    echo "submitted ${samplelist[$sample]} jobs!!"
+    echo "submitted ${sample} jobs!!"
     cd -
 done
